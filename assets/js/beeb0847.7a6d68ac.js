187f84143144fc99309be180f1f9b6b10cf20c38
Nightcycle Studios <coyer@nightcycle.us>
2022-01-06 17:12:14 -0500

    Built and published by Moonwave

---
+++ assets/js/beeb0847.7a6d68ac.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[3613],{97868:function(s){s.exports=JSON.parse('{"sourceUrl":"https://github.com/nightcycle/synthetic/blob/master","baseUrl":"/synthetic/","classOrder":[],"apiCategories":[]}')}}]);
