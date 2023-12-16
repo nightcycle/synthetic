f6195e9eee9a25d2c5c632d224902e9ee0414cab
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2023-12-16 02:24:34 -0500

    upgraded lsp

--- scripts/test/all.sh
+++ scripts/test/all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # sh scripts/test/download.sh
 # sh scripts/test/unit-test.sh
-sh scripts/test/lsp.sh src out
+luau-lsp analyze --sourcemap="sourcemap.json" --ignore="Packages/**" --ignore="src/Server/NPC/Animate.server.lua" --ignore="**/Packages/**" --ignore="*.spec.luau" --ignore="out/**" --flag:LuauTypeInferIterationLimit=0 --flag:LuauCheckRecursionLimit=0 --flag:LuauTypeInferRecursionLimit=0 --flag:LuauTarjanChildLimit=0 --flag:LuauTypeInferTypePackLoopLimit=0 --flag:LuauVisitRecursionLimit=0 --definitions=types/globalTypes.d.lua src
 # sh scripts/test/lsp.sh out src
 selene src
 # selene out
