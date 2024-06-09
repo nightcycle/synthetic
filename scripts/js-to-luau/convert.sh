2814c6517d8bc3b8c7de9b0f39fdd4ae17b95dc6
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2024-06-09 04:49:35 -0300

    Cleaned up workspace a bit + refactored Chip and ElevatedButton

--- scripts/js-to-luau/convert.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-js_to_lua="C:/Users/coyer/js-to-lua"
-target_dir="C:/Users/coyer/material-ui/packages/mui-material"
-synthetic="C:/Users/coyer/Documents/GitHub/synthetic"
-out="${synthetic}/src/mui-test"
-
-"${js_to_lua}/dist/apps/convert-js-to-lua/index.js" --input "${target_dir}/src/Badge/index.js" --output $out
